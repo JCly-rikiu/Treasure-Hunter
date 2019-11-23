d933947ff8e26638ac195349529d98c0466d3bc8
EJ17217 <j4595321@gmail.com>
2019-11-23 22:06:52 +0800

    Menu with IP scene

---
+++ Assets/Scripts/Menu/QuitMenu.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitMenu : MonoBehaviour
+{
+    public void QuitGame (){
+    	Application.Quit();
+    }
+}
