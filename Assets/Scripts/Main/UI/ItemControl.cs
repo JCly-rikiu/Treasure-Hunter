3edcf81f6817234e930acd1a3880b63e04963f0c
EJ17217 <j4595321@gmail.com>
2019-12-25 06:21:08 +0800

    GUI OK inventorySystem

---
+++ Assets/Scripts/Main/UI/ItemControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isSelected = false;
+    public bool isUsed = false;
+    public int type;
+    public void Used(){
+    	isUsed = true;
+    	Destroy(gameObject);
+    }
+    public void Select(){
+    	Debug.Log("touch isSelected");
+    	isSelected = true;
+    	gameObject.GetComponent<Image>().color = new Color(1f, 1f, 1f, 0.5f);
+    }
+    public void Unselect(){
+    	isSelected = false;
+    }
+    public int FetchType(){
+        return type;
+    }
+}
