f7933da7c7dcc5e383979476510ca3aa7fce5ae2
EJ17217 <j4595321@gmail.com>
2019-12-25 11:41:49 +0800

    UI controller

--- Assets/Scripts/Main/UI/UIController.cs
+++ Assets/Scripts/Main/UI/UIController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIController : MonoBehaviour
+public class UserInterface : MonoBehaviour
 {
     public RectTransform timebar;
     public Text timetext;
